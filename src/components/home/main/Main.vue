e3015cae4cabfed9a364c13d48327c3f7f45e15b
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-22 14:34:42 +0700

    chore: responsive main forfor mobile devices, sm devices, md devices, lg devices, xl devices and 2xl devices

---
+++ src/components/home/main/Main.vue
@@ -0,0 +1,73 @@
+<template>
+  <div
+    id="main"
+    class="mt-14 sm:mt-20 xl:mt-10 2xl:mt-0 relative overflow-hidden py-16 space-y-24"
+  >
+    <div class="relative">
+      <div
+        class="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8"
+      >
+        <div
+          class="mx-auto max-w-xl px-4 sm:px-6 lg:mx-0 lg:max-w-none lg:py-16 lg:px-0"
+        >
+          <div>
+            <div class="mt-6 lg:my-40">
+              <h2 class="text-3xl font-bold tracking-tight text-[#fafafa]">
+                <span class="text-[#407cff]">Control</span> your notifications
+              </h2>
+              <p class="mt-4 text-lg text-[#a1a1aa]">
+                We redesigned the inbox to work at a faster pace with direct
+                previews, shortcuts and filters. You will love getting
+                notifications again.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="mt-12 sm:mt-16 lg:mt-0">
+          <div
+            class="transition-opacity will-change-scroll duration-1000 -mr-48 pl-4 sm:pl-6 md:-mr-16 lg:relative lg:m-0 lg:h-full lg:px-0"
+          >
+            <img
+              class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+              src="../../../assets/inbox_dark.png"
+              alt="inbox"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="relative">
+      <div
+        class="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8"
+      >
+        <div
+          class="mx-auto max-w-xl px-4 sm:px-6 lg:col-start-2 lg:mx-0 lg:max-w-none lg:py-32 lg:px-0"
+        >
+          <div>
+            <div class="mt-6 lg:my-24">
+              <h2 class="text-3xl font-bold tracking-tight u-text-gray-900">
+                <span class="text-pink-400">Organize</span> your issues
+              </h2>
+              <p class="mt-4 text-lg text-[#a1a1aa]">
+                Issue boards with built-in automation for your GitHub
+                repositories. Linking a pull request to an issue moves it to
+                “in-review”. Moving an issue to “done” closes it, and much more.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="mt-12 sm:mt-16 lg:col-start-1 lg:mt-0">
+          <div
+            class="transition-opacity will-change-scroll duration-1000 -ml-48 pr-4 sm:pr-6 md:-ml-16 lg:relative lg:m-0 lg:h-full lg:px-0"
+          >
+            <img
+              class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
+              src="../../../assets/issues-dark.png"
+              alt="issues"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <template>
   <div
-    id="main"
+    id="main_container"
     class="min-h-[calc(100vh-4rem)] lg:min-h-[calc(100vh-5rem)] flex flex-col"
   >
     <BannerHero />
+    <Main />
   </div>
 </template>
