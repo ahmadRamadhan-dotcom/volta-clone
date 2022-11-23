79e052e046a86bbf2b849e38e0d28774ad5a59cd
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-23 10:09:25 +0700

    chore: responsive start now for mobile devices, sm devices, md devices, lg devices, xl devices and 2xl devices

--- src/components/home/startnow/Startnow.vue
+++ src/components/home/startnow/Startnow.vue
@@ -1,8 +1,31 @@
 <template>
   <div
-    class="xl:w-[1220px] mt-24 bg-[#ffffff1a] ring-1 ring-grey-200 z-[20] flex justify-center mx-auto rounded-lg h-[20em]"
+    class="w-11/12 xl:w-[1220px] mt-24 bg-[#ffffff1a] ring-1 ring-grey-200 z-[20] flex justify-center mx-auto rounded-lg h-[18em] lg:h-[20em]"
   >
-    <div class="border-8 grid place-content-center place-items-center"></div>
+    <div class="grid place-content-center place-items-center">
+      <p class="font-bold text-[#fafafa] text-3xl sm:text-4xl">
+        Start now with Volta
+      </p>
+      <p
+        class="text-[#a1a1aa] w-10/12 sm:w-full text-center font-normal text-lg mt-6"
+      >
+        Free for open-source and always will be.
+      </p>
+      <button
+        class="font-medium mt-8 focus:outline-none focus:ring-offset-black text-sm shadow-sm u-text-white bg-gradient-to-tr from-[#1a62ff] hover:bg-red-500 to-pink-500 transition-colors inline-flex items-center !p-[2px] !rounded-lg"
+        type="button"
+      >
+        <span
+          class="h-full w-[250px] sm:w-[320px] flex items-center justify-center bg-red-500 gap-2 backgroundGrey hover:opacity-80 z-[30] rounded-md text-base px-6 py-2 text-white transition-colors"
+        >
+          <a
+            href="https://github.com/apps/volta-net/installations/new"
+            target="_blank"
+            >Get Started</a
+          >
+        </span>
+      </button>
+    </div>
   </div>
 </template>
 
